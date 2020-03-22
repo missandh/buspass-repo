728369c0e7b2b368676c29e2faef1ccc1b8ab3e0
missandh <sandhya.misra@gmail.com>
2020-03-22 23:05:47 +0530

    Minor

--- src/org/buspass/route/Route.java
+++ src/org/buspass/route/Route.java
@@ -22,6 +22,8 @@ import org.buspass.connection.Connections;
  * 
  * viewAllRoutesWithStops(): To print and return all the routes along with their stop details
  * 
+ * viewTypeOfVehiclesPerRoute(): To print the type of vehicles, no of vehicles on each route
+ * 
  * getAvailabilityInRoute(int routeid) : To print available seats in this route. 
  * Difference of (sum of capacity of each bus on a route) - total active bus passes in this route
  * 
@@ -250,31 +252,4 @@ public class Route {
     	}
     }
     
-	/*
-	public void viewRoute(String originStop) throws SQLException {
-	    
-	    ArrayList<Integer> routeId = new ArrayList<Integer>();
-	    Connection con=Connections.makeConnection();
-	    String query="select routeid from BusPass.stop where stopname='HYD 13' and stoporder=1;";
-	    ResultSet result=Connections.sendQuery(con, "select routeid from BusPass.stop where stopname="+ originStop + " and stoporder=1 ;");
-	    //ResultSet result=Connections.sendQuery(con,query);
-	    while(result.next())
-	        routeId.add(result.getInt(1));
-	    if(routeId.isEmpty()) {
-	        System.out.println("No Bus is available from this stop");
-	        //Connections.closeConnection();
-	        return;
-	    }
-	    System.out.println(routeId.size());
-	    System.out.println(routeId);
-	    for(int i=0;i<routeId.size();i++) {
-	        String query1="select stoporder,stopname from BusPass.stop where routeid="+routeId.get(i) + ";";
-	        result=Connections.sendQuery(con,query1);
-	        
-	        System.out.println("Order" + " Stop Name     for route"+ (i+1) );
-	        while(result.next()) {
-	            System.out.println(result.getInt(1) + "     " + result.getString(2));            
-	        }        
-	    }
-	    */
 }
